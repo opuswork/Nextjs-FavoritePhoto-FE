1e4a7c16904916dfcafb5b932759d81ca000c6ce
김명환 <lostalgia.kr@gmail.com>
2026-01-12 15:49:17 +0900

    FE : 설정 파일 세팅 완료

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -3,12 +3,7 @@ import nextVitals from 'eslint-config-next/core-web-vitals';
 import prettier from 'eslint-config-prettier';
 
 export default defineConfig([
-  // 1. Next.js Core Web Vitals 규칙
   ...nextVitals,
-
-  // 2. Prettier와 충돌하는 ESLint 규칙 비활성화
   prettier,
-
-  // 3. Ignore 설정
   globalIgnores(['.next/**', 'out/**', 'build/**', 'node_modules/**']),
 ]);
