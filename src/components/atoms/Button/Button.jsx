c42c61c7eed1cf76c99bf48526f12280e30cbd83
Taeyoung <tseon@opuscore.net>
2026-01-13 10:47:26 +0900

    feat(button): add Button

--- src/components/atoms/Button/Button.jsx
+++ src/components/atoms/Button/Button.jsx
@@ -2,8 +2,9 @@
 "use client";
 
 import { useRouter } from "next/navigation";
+import { Styles } from "./Button.module.css";
 
 export default function Button({ children, href }) {
     const router = useRouter();
-    return <button onClick={() => router.push(href)}>{children}</button>;
+    return <button className={Styles.button} onClick={() => router.push(href)}>{children}</button>;
 }
