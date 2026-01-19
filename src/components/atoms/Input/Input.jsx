03ced0c1f057a71dde9c29aa12e9a93424edf6f8
Taeyoung <tseon@opuscore.net>
2026-01-19 18:15:24 +0900

    fixed: style import

--- src/components/atoms/Input/Input.jsx
+++ src/components/atoms/Input/Input.jsx
@@ -1,7 +1,20 @@
 // src/components/atoms/Input/Input.jsx
 "use client";
-import { Styles } from "./Input.module.css";
+import styles from "./Input.module.css";
 
-export default function Input({ type, placeholder, value, onChange }) {
-    return <input className={Styles.input} type={type} placeholder={placeholder} value={value} onChange={onChange} />;
+export default function Input({ 
+    type, 
+    placeholder, 
+    value, 
+    onChange 
+}) {
+    return (
+        <input 
+            className={styles.input} 
+            type={type} 
+            placeholder={placeholder} 
+            value={value} 
+            onChange={onChange} 
+        />
+    );
 }
