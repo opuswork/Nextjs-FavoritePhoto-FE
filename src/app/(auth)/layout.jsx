faa8efe3cd83bf55b92a64938627435fab984ffb
Taeyoung <tseon@opuscore.net>
2026-01-19 17:50:18 +0900

    refactor(app): remove (auth) route group

--- src/app/(auth)/layout.jsx
+++
@@ -1,7 +0,0 @@
-export default function AuthLayout({ children }) {
-  return (
-    <section style={{ minHeight: '100vh', display: 'grid', placeItems: 'center' }}>
-      {children}
-    </section>
-  );
-}
