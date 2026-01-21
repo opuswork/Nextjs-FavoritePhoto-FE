27511f937643ec17c911e22f2ee672140032cfe6
Taeyoung <tseon@opuscore.net>
2026-01-21 13:11:13 +0900

    feat: feature/label updated Label.jsx

--- src/components/atoms/Label/Label.jsx
+++ src/components/atoms/Label/Label.jsx
@@ -1,7 +1,18 @@
 // src/components/atoms/Label/Label.jsx
 "use client";
-import { Styles } from "./Label.module.css";
+import styles from "./Label.module.css";
 
-export default function Label({ children }) {
-    return <label className={Styles.label}>{children}</label>;
+export default function Label({ 
+    children, 
+    className, 
+    style 
+}) {
+    return (
+        <label 
+            className={`${styles.label ?? ''} ${className ?? ''}`.trim()} 
+            style={style}
+        >
+            {children}
+        </label>
+    );
 }
