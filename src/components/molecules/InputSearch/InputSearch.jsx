151c93ce21cd993983b05570d28335a70d8d9db1
Taeyoung <tseon@opuscore.net>
2026-01-21 14:45:51 +0900

    feat: feature/inputsearch update InputSearch.jsx

--- src/components/molecules/InputSearch/InputSearch.jsx
+++ src/components/molecules/InputSearch/InputSearch.jsx
@@ -1,46 +1,42 @@
 'use client';
 
-import styles from './InputSearch.module.css';
 import Input from '../../atoms/Input/Input';
 import Button from '../../atoms/Button/Button';
-import Image from 'next/image';
-import SearchIcon from '../../../public/assets/icons/ic_search.svg';
 
 export default function InputSearch({
     placeholder,
     value,
     onChange,
     className,
+    onClick,
     error,
     required,
     id,
     disabled,
-    icon = <Image src={SearchIcon} alt="search" width={20} height={20} />
+    icon = <img src="/assets/icons/ic_search.svg" alt="search" width={20} height={20} />
 }) {
     return (
-        <div className={`${styles.inputSearch} ${className}`}>
-            <Input 
-                type="text" 
-                placeholder={placeholder} 
-                value={value} 
-                onChange={onChange}
-                className={`${styles.input} ${className} ${error ? styles.error : ''}`}
-                error={error}
-                required={required}
-                id={id}
-                disabled={disabled}
-            />
-            <Button 
-                className={`${styles.button} ${className} ${error ? styles.error : ''}`} 
-                onClick={onClick}
-                error={error}
-                required={required}
-                id={id}
-                disabled={disabled}
-            >
-                {icon}
-            </Button>
-            {error && <p className={styles.errorMessage}>{error}</p>}
+        <div className={`flex flex-col ${className ?? ''}`}>
+            <div className="relative h-[50px] w-[320px]">
+                <Input 
+                    type="text" 
+                    placeholder={placeholder} 
+                    value={value} 
+                    onChange={onChange}
+                    className="h-full w-full rounded-sm border border-white/40 bg-black px-4 pr-[50px] text-[14px] text-white placeholder:text-white/40 focus:border-white focus:outline-none"
+                    required={required}
+                    id={id}
+                    disabled={disabled}
+                />
+                <Button 
+                    className="absolute right-0 top-0 flex h-full w-[50px] items-center justify-center rounded-r-sm border-l border-white/40 bg-black hover:bg-white/10"
+                    onClick={onClick}
+                    disabled={disabled}
+                >
+                    {icon}
+                </Button>
+            </div>
+            {error && <p className="mt-1 text-[12px] text-red-400">{error}</p>}
         </div>
     );
 }
